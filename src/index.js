0b302a5d630047273a5b01306f7f1105b0cb42e7
smit_mewada <smit.mewada@instinctinnovations.com>
2025-05-24 20:37:19 +0530

    Setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/server-config');
+const {sendBasicMail} = require('./services/email-service');
 
 const setUpAndStartServer = ()=>{
   app.listen(PORT, ()=>{
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-    
+
     console.log("Server started on port: ", PORT);
+
+    sendBasicMail(
+      'support@admin.com',
+      'mewadasmit72@gmail.com',
+      'Testing reminder service',
+      'Trying to send email through reminder service'
+    )
   });
 }
 
---
+++ src/services/email-service.js
@@ -0,0 +1,20 @@
+const sender = require('../config/email-config');
+
+const sendBasicMail =  async (mailFrom, mailTo, mailSubject, mailBody) =>{
+  try {
+    const response = await sender.sendMail({
+      from: mailFrom,
+      to: mailTo,
+      subject: mailSubject,
+      text: mailBody
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+module.exports = {
+  sendBasicMail
+}
